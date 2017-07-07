1a5a4c310875427d88fa305b6c7d466c28264396
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-07-07 11:42:03 -0400

    Adding new demo

---
+++ demo/site/src/main/java/org/onehippo/forge/gallerymagick/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.gallerymagick.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="gallerymagickdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
