391eaeb9beb9b84b8ad6735ecf8bd50fa798810c
Woonsan Ko <woonsan.ko@bloomreach.com>
2019-01-20 09:34:25 +0900

    HIPFORGE-253: upgrading for v13; removing old demo

--- demo/site/src/main/java/org/onehippo/forge/gallerymagick/demo/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.forge.gallerymagick.demo.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="gallerymagickdemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
